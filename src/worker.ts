9f047e78752af23ef1ce796c699070b784a0fdbf
Kaido Iwamoto <kaido@odiak.net>
2023-08-09 03:57:25 +0900

    Support username/password auth

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 import { Cookie, MemoryCookieStore } from 'tough-cookie'
 import { Fetcher } from './carelink'
 
@@ -11,37 +11,34 @@ export type Env = {
 
 const app = new Hono<{ Bindings: Env }>()
 
-app.post('/fetch', async (c) => {
-  const password = c.req
-    .header('Authorization')
-    ?.match(/^Bearer\s+(.+?)\s*$/)?.[1]
+app.post(
+  '/fetch',
+  authWrapper(async (credentials, c) => {
+    const passwordHash = await sha1(
+      `${credentials.username}@${credentials.password}`,
+    )
+    const { cookieStore, saveCookies } = await setupCookie(
+      c.env.COOKIES,
+      passwordHash,
+    )
 
-  if (password === undefined || password !== c.env.PASSWORD) {
-    return c.text('Unauthorized', 401)
-  }
+    const fetcher = new Fetcher(cookieStore, {
+      CARELINK_USERNAME: credentials.username,
+      CARELINK_PASSWORD: credentials.password,
+      COUNTRY: credentials.country,
+      LANG: credentials.language,
+    })
+    const res = await fetcher.fetchData()
 
-  const passwordHash = await sha1(password)
-  const { cookieStore, saveCookies } = await setupCookie(
-    c.env.COOKIES,
-    passwordHash,
-  )
+    await saveCookies()
 
-  const fetcher = new Fetcher(cookieStore, {
-    CARELINK_USERNAME: c.env.MINIMED_USER,
-    CARELINK_PASSWORD: c.env.MINIMED_PASSWORD,
-    COUNTRY: 'jp',
-    LANG: 'ja',
-  })
-  const res = await fetcher.fetchData()
+    if (res === undefined) {
+      return c.json({ error: 'Failed to fetch data' }, 500)
+    }
 
-  await saveCookies()
-
-  if (res === undefined) {
-    return c.json({ error: 'Failed to fetch data' }, 500)
-  }
-
-  return res
-})
+    return res
+  }),
+)
 
 export default app
 
@@ -70,4 +67,57 @@ async function setupCookie(kv: KVNamespace, key: string) {
   }
 
   return { cookieStore, saveCookies }
+}
+
+type Credentials = {
+  username: string
+  password: string
+  language: string
+  country: string
+}
+
+function authWrapper(
+  f: (
+    credentials: Credentials,
+    c: Context<{ Bindings: Env }>,
+  ) => Promise<Response>,
+): (c: Context<{ Bindings: Env }>) => Promise<Response> {
+  return async (c) => {
+    const legacyPassword = c.req
+      .header('Authorization')
+      ?.match(/^Bearer\s+(.+?)\s*$/)?.[1]
+    if (legacyPassword !== undefined) {
+      if (legacyPassword === c.env.PASSWORD) {
+        return f(
+          {
+            username: c.env.MINIMED_USER,
+            password: c.env.MINIMED_PASSWORD,
+            language: 'ja',
+            country: 'jp',
+          },
+          c,
+        )
+      } else {
+        return c.text('Unauthorized', 401)
+      }
+    }
+
+    let credentials: Credentials
+
+    try {
+      credentials = await c.req.json<Credentials>()
+      if (
+        !credentials.username ||
+        !credentials.password ||
+        !credentials.country ||
+        !credentials.language
+      ) {
+        return c.text('Unauthorized', 401)
+      }
+    } catch (e) {
+      return c.text('Unauthorized', 401)
+    }
+
+    return f(credentials, c)
+  }
 }
